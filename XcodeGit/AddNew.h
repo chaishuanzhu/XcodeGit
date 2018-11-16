6c145e698bd5927d16d64c276ced7cf887d42ab1
飞鱼 <shibasenhashira@chaishuanzhudemacbook-pro.local>
2018-11-16 13:27:29 +0800

    add test

---
+++ XcodeGit/AddNew.h
@@ -0,0 +1,17 @@
+//
+//  AddNew.h
+//  XcodeGit
+//
+//  Created by 飞鱼 on 2018/11/16.
+//  Copyright © 2018 RongZhiXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddNew : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
